ffdf8614368412a4f6502107cd8d743b140ea3e1
SuzukiSora <134992801+suzukisora0514@users.noreply.github.com>
2023-07-13 11:55:22 +0900

    Lastwork3_team4
    追加しました

---
+++ Lastwork3_team4/Lastwork3_team4/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Lastwork3_team4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "スレッドの作成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(66, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(310, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "返信する";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(310, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "スレッドの表示";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(66, 419);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(310, 42);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ログイン";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(66, 546);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(310, 41);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "ログアウト";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(75, 683);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(82, 34);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "閉じる";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(677, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "名前を入力してください";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(680, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            this.textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(634, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(302, 25);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Visible = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(725, 215);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 38);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "決定";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(634, 228);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(302, 25);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(558, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "タイトル：";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(575, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "本文：";
+            this.label3.Visible = false;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(861, 309);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 34);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "決定";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Visible = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(651, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 18);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "どのタイトルに返信しますか？";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(654, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 776);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lastwork3_team4/Lastwork3_team4/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Lastwork3_team4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //スレッドの作成
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            button8.Visible = true;
+
+
+
+            string connectionString = "Data Source=BBS;Version=3;";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                // テーブルに新しいカラムを追加する ALTER TABLE 文の実行
+                using (SQLiteCommand alterCommand = new SQLiteCommand(connection))
+                {
+                    alterCommand.CommandText = "CREATE TABLE IF NOT EXISTS Bbs (title TEXT);";
+                    alterCommand.ExecuteNonQuery();
+
+                   // alterCommand.CommandText = "ALTER TABLE Bbs ADD COLUMN title TEXT;";
+                   // alterCommand.ExecuteNonQuery();
+                }
+
+                // テキストボックスから入力された値を挿入する INSERT 文の実行
+                string columnName = "title";
+                string columnValue = textBox2.Text;
+
+                using (SQLiteCommand insertCommand = new SQLiteCommand(connection))
+                {
+                    insertCommand.CommandText = "INSERT INTO Bbs (" + columnName + ") VALUES (@value);";
+                    insertCommand.Parameters.AddWithValue("@value", columnValue);
+                    insertCommand.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+            //明日の自分へ
+            //上のコードはタイトルしか出来てない、しかもエラー出る。タイトル：textbox2、本文：textbox3
+
+
+
+            /*
+            var sqlConnectionSb = new SQLiteConnectionStringBuilder { DataSource = "BBS.db" };
+            using (var cn = new SQLiteConnection(sqlConnectionSb.ToString()))
+            {
+                cn.Open();
+                using (var cmd = new SQLiteCommand(cn))
+                {
+                    //テーブル追加
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS Bbs(" + "name TEXT," + "birthday TEXT" + ")";
+                    cmd.ExecuteNonQuery();
+                    //データ追加
+                    string a = textBox1.Text;
+                    cmd.CommandText = "INSERT INTO Bbs VALUES(@name);";
+                    cmd.Parameters.AddWithValue("@name", a);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            */
+        }
+
+        //返信する
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label4.Visible = true;
+
+            //タイトル一覧出すコード
+            var sqlConnectionSb = new SQLiteConnectionStringBuilder { DataSource = "BBS.db" };
+            using (var cn = new SQLiteConnection(sqlConnectionSb.ToString()))
+            {
+                cn.Open();
+                using (var cmd = new SQLiteCommand(cn))
+                {
+                    cmd.CommandText = "SELECT * FROM Bbs;";
+
+                    label5.Text = cmd.CommandText;
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var sb = new StringBuilder();
+                        while (reader.Read())
+                        {
+                            sb.AppendLine(reader.GetString(0)); // title 列の値を取得する
+                        }
+                        label5.Text = sb.ToString();
+                    }
+                }
+            }    
+
+            label5.Visible = true;
+        }
+        //スレッドの表示
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+        //ログイン
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            textBox1.Visible = true;
+            button7.Visible = true;
+            var sqlConnectionSb = new SQLiteConnectionStringBuilder { DataSource = "BBS.db" };
+            using (var cn = new SQLiteConnection(sqlConnectionSb.ToString()))
+            {
+                cn.Open();
+                using (var cmd = new SQLiteCommand(cn))
+                {
+                    //テーブル作成
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS Bbs(" + "name TEXT" + ")";
+                    cmd.ExecuteNonQuery();
+                    //データ追加
+                    string a = textBox1.Text;
+                    cmd.CommandText = "INSERT INTO Bbs VALUES(@name);";
+                    cmd.Parameters.AddWithValue("@name", a);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        //ログアウト
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+        //終了
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            textBox1.Visible = false;
+            button7.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            button8.Visible = false;
+        }
+    }
+}
